348f3586d634a5be2e6080f16371cb34ae9a9207
xy98 <xy98@global.tesco.org>
2016-05-29 10:33:49 +0530

    adding solution

---
+++ LogProcessor/LogProcessor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string tempLineTimeStamp="";
+            string tempLog = "";
+            int numLinesRead = 0;
+            int numLinesWrite = 0;
+            string outputDir=@"D:\";
+
+                Parallel.ForEach( Directory.EnumerateFiles(@outputDir, "Log3*.txt", SearchOption.TopDirectoryOnly), (file) =>
+                {
+                    
+                    foreach (string line in File.ReadLines(file))
+                    {
+
+                        numLinesRead++;
+                        string NewFile = outputDir + "Processed_" + Path.GetFileName(file);
+                        if (line.Contains("Timestamp"))
+                        {
+                            tempLineTimeStamp = line;
+                        }
+                        if (line.Contains("TracerExit Machine"))
+                        {
+                            tempLog = tempLineTimeStamp + line + tempLog + Environment.NewLine;
+                            File.AppendAllText(@NewFile, tempLog);
+                            numLinesWrite++;
+
+                        }
+                        if (line.Contains("End Trace"))
+                        {
+                            tempLog = line;
+                        }
+                    }
+                });
+
+        }
+    }
+}
---
+++ LogProcessor/LogProcessorUnitTest/LogProcessorTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogProcessor;
+
+namespace LogProcessorUnitTest
+{
+    [TestClass]
+    public class LogProcessorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hi", "Hi");
+        }
+    }
+}
